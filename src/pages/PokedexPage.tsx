c3b48dd030090fed27e8b4af109486256ee94d53
David Bernstein <susuvnkl6@gmail.com>
2024-03-19 14:18:17 +0200

    Pokedex page

---
+++ src/pages/PokedexPage.tsx
@@ -0,0 +1,28 @@
+import FilterPokemons from "@/components/ui/FilterPokemons";
+import PokemonGrid from "@/components/ui/PokemonsGrid";
+import { Filter } from "@/hooks/usePokemonList";
+import { useState } from "react";
+
+function PokedexPage() {
+  const [filter, setFilter] = useState<Filter>({
+    name: "",
+    id: "",
+    type: "",
+    gender: "",
+    color: "",
+    ability: "",
+  });
+  return (
+    <div className="flex flex-col items-center">
+      <div className="w-full">
+        <FilterPokemons setFilter={setFilter} />
+      </div>
+      <div>
+        <PokemonGrid filter={filter} />
+      </div>
+      <div>Scroll</div>
+    </div>
+  );
+}
+
+export default PokedexPage;
