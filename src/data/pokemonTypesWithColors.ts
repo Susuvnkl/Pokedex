718c79b782167f90758a90706118a8a88a227659
David Bernstein <susuvnkl6@gmail.com>
2024-03-19 14:05:45 +0200

    pokemonts data

---
+++ src/data/pokemonTypesWithColors.ts
@@ -0,0 +1,108 @@
+export type PokemonTypeColor = {
+  name: string;
+  color: string;
+  url: string;
+};
+
+export const pokemonTypesWithColors: PokemonTypeColor[] = [
+  {
+    name: "normal",
+    color: "#A8A77A",
+    url: "https://pokeapi.co/api/v2/type/1/",
+  },
+  {
+    name: "fighting",
+    color: "#C22E28",
+    url: "https://pokeapi.co/api/v2/type/2/",
+  },
+  {
+    name: "flying",
+    color: "#A98FF3",
+    url: "https://pokeapi.co/api/v2/type/3/",
+  },
+  {
+    name: "poison",
+    color: "#A33EA1",
+    url: "https://pokeapi.co/api/v2/type/4/",
+  },
+  {
+    name: "ground",
+    color: "#E2BF65",
+    url: "https://pokeapi.co/api/v2/type/5/",
+  },
+  {
+    name: "rock",
+    color: "#B6A136",
+    url: "https://pokeapi.co/api/v2/type/6/",
+  },
+  {
+    name: "bug",
+    color: "#A6B91A",
+    url: "https://pokeapi.co/api/v2/type/7/",
+  },
+  {
+    name: "ghost",
+    color: "#735797",
+    url: "https://pokeapi.co/api/v2/type/8/",
+  },
+  {
+    name: "steel",
+    color: "#B7B7CE",
+    url: "https://pokeapi.co/api/v2/type/9/",
+  },
+  {
+    name: "fire",
+    color: "#EE8130",
+    url: "https://pokeapi.co/api/v2/type/10/",
+  },
+  {
+    name: "water",
+    color: "#6390F0",
+    url: "https://pokeapi.co/api/v2/type/11/",
+  },
+  {
+    name: "grass",
+    color: "#7AC74C",
+    url: "https://pokeapi.co/api/v2/type/12/",
+  },
+  {
+    name: "electric",
+    color: "#F7D02C",
+    url: "https://pokeapi.co/api/v2/type/13/",
+  },
+  {
+    name: "psychic",
+    color: "#F95587",
+    url: "https://pokeapi.co/api/v2/type/14/",
+  },
+  {
+    name: "ice",
+    color: "#96D9D6",
+    url: "https://pokeapi.co/api/v2/type/15/",
+  },
+  {
+    name: "dragon",
+    color: "#6F35FC",
+    url: "https://pokeapi.co/api/v2/type/16/",
+  },
+  {
+    name: "dark",
+    color: "#705746",
+    url: "https://pokeapi.co/api/v2/type/17/",
+  },
+  {
+    name: "fairy",
+    color: "#D685AD",
+    url: "https://pokeapi.co/api/v2/type/18/",
+  },
+  {
+    name: "unknown",
+    color: "#68A090",
+    url: "https://pokeapi.co/api/v2/type/10001/",
+  },
+  {
+    name: "shadow",
+    color: "#604E82",
+    url: "https://pokeapi.co/api/v2/type/10002/",
+  },
+];
---
+++ src/data/pokemonsColor.ts
@@ -0,0 +1,17 @@
+export type PokemonColor = {
+  name: string;
+  url: string;
+};
+
+export const pokemonColors: PokemonColor[] = [
+  { name: "black", url: "https://pokeapi.co/api/v2/pokemon-color/1/" },
+  { name: "blue", url: "https://pokeapi.co/api/v2/pokemon-color/2/" },
+  { name: "brown", url: "https://pokeapi.co/api/v2/pokemon-color/3/" },
+  { name: "gray", url: "https://pokeapi.co/api/v2/pokemon-color/4/" },
+  { name: "green", url: "https://pokeapi.co/api/v2/pokemon-color/5/" },
+  { name: "pink", url: "https://pokeapi.co/api/v2/pokemon-color/6/" },
+  { name: "purple", url: "https://pokeapi.co/api/v2/pokemon-color/7/" },
+  { name: "red", url: "https://pokeapi.co/api/v2/pokemon-color/8/" },
+  { name: "white", url: "https://pokeapi.co/api/v2/pokemon-color/9/" },
+  { name: "yellow", url: "https://pokeapi.co/api/v2/pokemon-color/10/" },
+];
