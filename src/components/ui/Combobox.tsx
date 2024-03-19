2e6519a3ea63af2453feeaaca51cfd3ef44ab300
David Bernstein <susuvnkl6@gmail.com>
2024-03-19 14:14:26 +0200

    Filter pokemons

---
+++ src/components/ui/Combobox.tsx
@@ -0,0 +1,87 @@
+import { CaretSortIcon, CheckIcon } from "@radix-ui/react-icons";
+
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+} from "@/components/ui/command";
+import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover";
+import { useEffect, useState } from "react";
+
+export type ComboboxOption = {
+  value: string;
+  label: string;
+};
+
+interface ComboboxProps {
+  options: ComboboxOption[];
+  label: string;
+  onChange: (value: string) => void;
+  selectedValue: string;
+}
+
+function Combobox(props: ComboboxProps) {
+  const { options, label, onChange, selectedValue } = props;
+  const [open, setOpen] = useState(false);
+  const [value, setValue] = useState("");
+
+  const handleChange = (currentValue: string) => {
+    setValue(currentValue);
+    onChange(currentValue); // Call the onChange prop function
+  };
+
+  useEffect(() => {
+    setValue(selectedValue);
+  }, [selectedValue]);
+
+  return (
+    <Popover open={open} onOpenChange={setOpen}>
+      <PopoverTrigger asChild>
+        <Button
+          variant="outline"
+          role="combobox"
+          aria-expanded={open}
+          className="w-full justify-between"
+        >
+          {value ? options.find((option) => option.value === value)?.label : `Select ${label}...`}
+          <CaretSortIcon className="ml-2 h-4 w-4 shrink-0 opacity-50" />
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent className="w-full p-0">
+        <Command>
+          <CommandInput placeholder={`Select ${label}...`} className="h-9" />
+          <CommandList>
+            <CommandEmpty>No {label} found.</CommandEmpty>
+            <CommandGroup>
+              {options.map((option) => (
+                <CommandItem
+                  key={option.value}
+                  value={option.value}
+                  onSelect={(currentValue) => {
+                    handleChange(currentValue === value ? "" : currentValue);
+                    setOpen(false);
+                  }}
+                >
+                  {option.label}
+                  <CheckIcon
+                    className={cn(
+                      "ml-auto h-4 w-4",
+                      value === option.value ? "opacity-100" : "opacity-0"
+                    )}
+                  />
+                </CommandItem>
+              ))}
+            </CommandGroup>
+          </CommandList>
+        </Command>
+      </PopoverContent>
+    </Popover>
+  );
+}
+
+export default Combobox;
---
+++ src/components/ui/FilterPokemons.tsx
@@ -0,0 +1,144 @@
+import React, { useMemo, useState } from "react";
+import { Filter } from "@/hooks/usePokemonList"; // Adjust the import path as needed
+import { Switch } from "@/components/ui/switch";
+import { Button } from "./button";
+import Combobox, { ComboboxOption } from "./Combobox";
+import { pokemonTypesWithColors } from "@/data/pokemonTypesWithColors";
+import { pokemonColors } from "@/data/pokemonsColor";
+import { pokemonAbilities } from "@/data/pokemonAbilities";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+
+interface FilterPokemonsProps {
+  setFilter: (filter: Filter) => void;
+}
+const initialState: Filter = {
+  name: "",
+  id: "",
+  type: "",
+  gender: "",
+  color: "",
+  ability: "",
+};
+
+function FilterPokemons({ setFilter }: FilterPokemonsProps) {
+  // Temporary state to hold input values before applying them
+  const [filterState, setFilterState] = useState<Filter>(initialState);
+  const [advancedFilter, setAdvancedFilter] = useState<boolean>(false);
+
+  // Process input value to handle numeric and string values accordingly
+  function processInputValue(value: string): string {
+    if (/^0\d+$/.test(value)) {
+      // If numeric with leading zeros
+      return parseInt(value, 10).toString();
+    } else if (/^\d+$/.test(value)) {
+      // If purely numeric
+      return value;
+    } else {
+      // For strings
+      return value.toLowerCase().replace(/\s+/g, "-");
+    }
+  }
+
+  // Handle changes in the input field for name/id
+  const handleQueryChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const processedValue = processInputValue(e.target.value);
+    if (/^\d+$/.test(processedValue)) {
+      setFilterState((prev) => ({ ...prev, id: processedValue, name: "" }));
+    } else {
+      setFilterState((prev) => ({ ...prev, name: processedValue, id: "" }));
+    }
+  };
+
+  // Handle changes from Combobox components
+  const handleComboboxChange = (field: keyof Filter, value: string) => {
+    setFilterState((prev) => ({ ...prev, [field]: value }));
+  };
+
+  // Apply filters and reset state
+  const applyFilters = () => {
+    setFilter(filterState);
+    setFilterState(initialState);
+  };
+
+  // Generate lists for Combobox components
+  const generateComboboxList = (data: { name: string }[]) => {
+    return data.map((item) => ({
+      value: item.name,
+      label: item.name.charAt(0).toUpperCase() + item.name.slice(1),
+    }));
+  };
+
+  const typesList = useMemo(() => generateComboboxList(pokemonTypesWithColors), []);
+  const colorsList = useMemo(() => generateComboboxList(pokemonColors), []);
+  const abilityList = useMemo(() => generateComboboxList(pokemonAbilities), []);
+
+  const genderList: ComboboxOption[] = [
+    { value: "female", label: "Female" },
+    { value: "male", label: "Male" },
+  ];
+
+  return (
+    <div className="w-full flex flex-col justify-center items-center">
+      <div className="lg:w-3/5 md:w-4/5 p-5  ">
+        <img className="w-full" src="/utils/logos/Pokemon.svg" alt="Pokemon Logo" />
+      </div>
+      <div>
+        <div className="flex flex-row	justify-between">
+          <Label>
+            {advancedFilter ? "Advanced: Filter by Name or ID" : "Basic: Filter by Name or ID"}
+          </Label>
+          <Switch
+            checked={advancedFilter}
+            onCheckedChange={() => setAdvancedFilter(!advancedFilter)}
+          />
+        </div>
+        <div className="flex flex-row">
+          <Input
+            id="pokemonQueryFilter"
+            type="text"
+            value={filterState.name || filterState.id}
+            onChange={handleQueryChange}
+            placeholder="Type to filter..."
+          ></Input>
+          <Button className="ml-2 p-3" onClick={applyFilters}>
+            🔍
+          </Button>
+          <Button className="ml-5" onClick={applyFilters}>
+            Discover
+          </Button>
+        </div>
+        {advancedFilter && (
+          <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-4 gap-5 justify-items-center p-3">
+            <Combobox
+              onChange={(value) => handleComboboxChange("gender", value)}
+              options={genderList}
+              selectedValue={filterState.gender}
+              label="Gender"
+            />
+            <Combobox
+              onChange={(value) => handleComboboxChange("type", value)}
+              options={typesList}
+              selectedValue={filterState.type}
+              label="Type"
+            />
+            <Combobox
+              onChange={(value) => handleComboboxChange("ability", value)}
+              options={abilityList}
+              selectedValue={filterState.ability}
+              label="Ability"
+            />
+            <Combobox
+              onChange={(value) => handleComboboxChange("color", value)}
+              options={colorsList}
+              selectedValue={filterState.color}
+              label="Color"
+            />
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default FilterPokemons;
