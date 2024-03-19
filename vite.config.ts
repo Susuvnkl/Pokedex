e0fc00605a723d5b86c8652393ec6f81131d2434
David Bernstein <susuvnkl6@gmail.com>
2024-03-19 15:49:03 +0200

    PWA config

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,53 @@
 import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      // workbox: {
+      //   globPatterns: ['**/*.{js,css,html,ico,png,svg}']
+      // },
+      includeAssets: ["Poké_Ball_icon.png", "robots.txt", "apple-touch-icon.png"],
+      manifest: {
+        name: "Pokedex",
+        short_name: "Pokedex By David Bernstein",
+        icons: [
+          {
+            src: "/pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "any",
+          },
+          {
+            src: "/pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any",
+          },
+          {
+            src: "/pwa-maskable-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "maskable",
+          },
+          {
+            src: "/pwa-maskable-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable",
+          },
+        ],
+        start_url: "/",
+        display: "standalone",
+        background_color: "#FFFFFF",
+        theme_color: "#000000",
+      },
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
