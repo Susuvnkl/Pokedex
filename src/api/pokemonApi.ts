4e10398dd8ad1a1ebb18a0f8dc1fcf86ffaed84b
David Bernstein <susuvnkl6@gmail.com>
2024-03-19 14:06:52 +0200

    use pokemon list hook

---
+++ src/api/pokemonApi.ts
@@ -0,0 +1,100 @@
+import axios from "axios";
+
+const BASE_URL = import.meta.env.VITE_BASE_URL;
+
+export interface PokemonDetail {
+  id: string;
+  name: string;
+  sprites: {
+    other: {
+      "official-artwork": { front_default: string };
+    };
+  };
+  types: { type: { name: string } }[];
+}
+export type genderDetail = {
+  pokemon_species: {
+    name: string;
+    url: string;
+  };
+  rate: number;
+};
+
+const limit: number = 20;
+
+async function fetchFromUrl<T>(url: string): Promise<T> {
+  try {
+    const response = await axios.get<T>(url);
+    return response.data;
+  } catch (error) {
+    console.error("Failed to fetch from URL", url, error);
+    throw new Error(`Failed to fetch data: ${error}`);
+  }
+}
+
+async function fetchResource<T>(
+  resource: string,
+  query: string | number,
+  queryParams: string = ""
+): Promise<T> {
+  const url = `${BASE_URL}/${resource}/${query}${queryParams ? `?${queryParams}` : ""}`;
+  return fetchFromUrl<T>(url);
+}
+
+export const fetchPokemons = async (): Promise<PokemonDetail[]> => {
+  const queryParams = `limit=${limit}`;
+  const { results } = await fetchResource<{ results: { name: string; url: string }[] }>(
+    "pokemon",
+    "",
+    queryParams
+  );
+
+  return Promise.all(results.map((pokemon) => fetchFromUrl<PokemonDetail>(pokemon.url)));
+};
+
+export const fetchPokemonByNameOrId = (query: string | number): Promise<PokemonDetail> => {
+  return fetchResource<PokemonDetail>("pokemon", query);
+};
+
+export const fetchPokemonsByType = async (typeName: string): Promise<PokemonDetail[]> => {
+  const { pokemon } = await fetchResource<{ pokemon: { pokemon: { name: string } }[] }>(
+    "type",
+    typeName
+  );
+  return Promise.all(pokemon.map((entry) => fetchPokemonByNameOrId(entry.pokemon.name)));
+};
+
+// Example for fetching based on gender, assuming API supports it directly
+export const fetchPokemonsByGender = async (genderName: string): Promise<PokemonDetail[]> => {
+  const pokemons = await fetchResource<genderDetail[]>("gender", genderName);
+  console.log("test", pokemons.pokemon_species_details);
+  return Promise.all(
+    pokemons.pokemon_species_details.map((entry: genderDetail) =>
+      fetchPokemonByNameOrId(entry.pokemon_species.name)
+    )
+  );
+};
+
+// Fetch Pokémons by color
+export const fetchPokemonsByColor = async (colorName: string): Promise<PokemonDetail[]> => {
+  const pokemon = await fetchResource<{ pokemon: { pokemon: { name: string } }[] }>(
+    "pokemon-color",
+    colorName
+  );
+  console.log(pokemon);
+  return Promise.all(pokemon.pokemon_species.map((entry) => fetchPokemonByNameOrId(entry.name)));
+};
+
+// Fetch Pokémons by ability
+export const fetchPokemonsByAbility = async (abilityName: string): Promise<PokemonDetail[]> => {
+  const { pokemon } = await fetchResource<{ pokemon: { pokemon: { name: string } }[] }>(
+    "ability",
+    abilityName
+  );
+  return Promise.all(pokemon.map((entry) => fetchPokemonByNameOrId(entry.pokemon.name)));
+};
+
+// Assuming fetchEvolutionChainById follows a similar pattern
+export const fetchEvolutionChainById = (id: string | number): Promise<any> => {
+  return fetchResource("pokemon-species", id, "evolution_chain");
+};
---
+++ src/hooks/usePokemonList.ts
@@ -0,0 +1,58 @@
+import { useQuery } from "@tanstack/react-query";
+import {
+  PokemonDetail,
+  fetchPokemonByNameOrId,
+  fetchPokemons,
+  fetchPokemonsByType,
+  fetchPokemonsByGender,
+  fetchPokemonsByAbility,
+  fetchPokemonsByColor,
+} from "@/api/pokemonApi";
+
+export type Filter = {
+  name: string;
+  id: string;
+  type: string;
+  gender: string;
+  ability: string;
+  color: string;
+};
+
+export const usePokemonList = (filter: Filter) => {
+  // Dynamic fetch function selection based on the filter provided
+  const fetchFilteredPokemons = async (): Promise<PokemonDetail[]> => {
+    // Handle name filter (assuming this might directly match ID or name)
+    if (filter.name) {
+      return [await fetchPokemonByNameOrId(filter.name || filter.id)];
+    }
+
+    // Handle other filters
+    if (filter.type) {
+      return await fetchPokemonsByType(filter.type);
+    }
+    if (filter.gender) {
+      return await fetchPokemonsByGender(filter.gender);
+    }
+    if (filter.ability) {
+      return await fetchPokemonsByAbility(filter.ability);
+    }
+    if (filter.color) {
+      return await fetchPokemonsByColor(filter.color);
+    }
+
+    // Default fetch if no specific filter is applied
+    return await fetchPokemons();
+  };
+
+  // React Query to manage fetching, caching, and updating the pokemons list
+  const {
+    data: pokemons,
+    isLoading,
+    error,
+  } = useQuery({
+    queryKey: ["pokemon", filter],
+    queryFn: fetchFilteredPokemons,
+  });
+
+  return { pokemons, isLoading, error };
+};
