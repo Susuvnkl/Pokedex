4a9f8ce8db06994f7fc03623ffdfc2e68dd62f6b
David Bernstein <susuvnkl6@gmail.com>
2024-03-20 16:15:25 +0200

    pre load backgounds

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -1,7 +1,22 @@
+import { useEffect } from "react";
 import { ModeToggle } from "@/components/ui/ModeToggle";
 import { Outlet } from "react-router-dom";
 
+function preLoadImages(images: any[]) {
+  images.forEach((image) => {
+    const img = new Image();
+    img.src = image;
+  });
+}
+
 function Layout() {
+  useEffect(() => {
+    preLoadImages([
+      "/utils/pictures/background-dark.webp",
+      "/utils/pictures/background-bright.webp",
+    ]);
+  }, []);
+
   return (
     <div
       className="min-h-screen bg-no-repeat bg-cover bg-center bg-fixed
